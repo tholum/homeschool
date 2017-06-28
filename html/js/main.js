05872cc67f050baa501f30a691f42a68d798d158
tholum <tholum@couleetechlink.com>
2017-06-28 13:56:44 -0500

    First Homeschool App Push

---
+++ html/js/main.js
@@ -0,0 +1,75 @@
+function Child(info){
+    var self = this;
+    self.name = ko.observable( info.name );
+    self.color = ko.observable( info.color );
+    self.classes = ko.observableArray(info.classes );
+    self.progress = ko.observableArray( info.progress );
+    self.class = ko.observable( info.classes[0] );
+    self.classDays = ko.observableArray([]);
+    self.data = ko.observable( );
+
+    self.day = ko.observable( false );
+    self.getData = function(){
+        $.getJSON("data/" + self.class() + ".json" , function(data){
+            var classDays = [];
+            $.each( data , function( key , day ){
+                classDays.push( day );
+                console.log( day );
+            });
+            self.classDays( classDays );
+            self.data(data);
+        });
+    };
+
+    self.toJson = function(){
+        return {
+            "name" : self.name(),
+            "classes" : self.classes(),
+            "progress" : self.progress(),
+            "color" : self.color()
+        };
+    };
+}
+
+function main(){
+    var self = this;
+    self.child = ko.observable( false );
+    self.child.subscribe( function(c){
+        console.log( c );
+    });
+    self.children = ko.observableArray();
+    self.updateInfo = function(){
+        var children = JSON.parse( localStorage.getItem( "children" ) );
+        if( children ){
+            var cleanChildren = [];
+            children.forEach( function( child ){
+                cleanChildren.push( new Child( child ) );
+            });
+            self.children(cleanChildren );
+        }
+    };
+    self.save = function(){
+        var cleanChildren = [];
+        self.children().forEach( function( child ){
+            cleanChildren.push( child.toJson() );
+        });
+        localStorage.setItem( "children" , JSON.stringify( cleanChildren ) );
+    };
+    self.addChild = function( child ){
+        var base = {
+            "name" : "",
+            "color" : "",
+            "classes" : [],
+            "progress" : []
+        };
+        if( typeof child == "object" && child !== null ){
+            child = $.extend( base , child );
+        } else {
+            child = base;
+        }
+        self.children.push( new Child( child ) );
+        setTimeout( self.save );
+
+    };
+    self.updateInfo();
+}
---
+++ index.js
@@ -0,0 +1,79 @@
+var baseUrl = "https://allinonehomeschool.com/"
+var jsdom = require("node-jsdom");
+var fs = require('fs');
+
+var subpage = 'getting-ready-2/';
+var days = {};
+function cleanDayText( str ){
+    return str.replace(/^(Day [0-9]{1,3})(.*)/ , "$1").replace( /\r?\n|\r/g , "" );
+}
+function matchesDay( str ){
+    return str.match(/^Day [0-9]{1,3}/)
+}
+
+jsdom.env(
+          baseUrl + subpage ,
+            ["http://code.jquery.com/jquery.js"],
+              function (errors, window) {
+                  window.$('ol').each( function( ){
+                      var self = this;
+                      var DayFound = false;
+                      var Day = "";
+                      var getDayChecks = [
+                          function( ){
+                              return String( window.$(self).prev("p").text() );
+                          },
+                          function( ){
+                              return String( window.$(self).prev("div").text() );
+                          },
+                          function( ){
+                              return String( window.$(self).parent().prev("div").text() );
+                          } ,
+                          function( ){
+                              return String( window.$(self).parent().prev("div").find("strong").last().text() );
+                          },
+                          function( ){
+                              return String( window.$(self).parent().prev("div").children("div").text() );
+                          }
+                      ];
+
+                      getDayChecks.forEach( function(func){
+                          if( !DayFound ){
+                              var dayToCheck = func( window , this );
+                              if( matchesDay(dayToCheck ) ){
+                                  DayFound = true;
+                                  Day = cleanDayText( dayToCheck );
+                              }
+                          }
+                      });
+                      if( DayFound ){
+                          var i = 1;
+                          var dayInt = Day.replace(/[^0-9]/g , "" );
+                          days[dayInt] = {
+                              "title" : Day,
+                              "day" : dayInt,
+                              "items" : []
+                          };
+                          window.$(this).children().each( function(){
+                              var item = {
+                                  "text" : window.$(this).text(),
+                                  "html" : window.$(this).html(),
+                                  "links" : []
+                              };
+                              window.$(this).children("a").each( function(){
+                                  item.links.push(window.$(this).attr('href'));
+                              });
+                              days[dayInt].items.push( item );
+                          });
+                      }
+                } );
+                console.log( days );
+                fs.writeFile("days.json", JSON.stringify( days ) , function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+
+                    console.log("The file was saved!");
+                });
+            }
+ );
